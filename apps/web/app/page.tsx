5b98312a1bfffb8b5a2828fff1a5a23f71ac0620
Rio-awsm <mm.saha1982@gmail.com>
2025-08-19 19:16:35 +0530

    feat: integrate Convex backend with workspace structure
    - Added Convex functions and schema definitions in the backend package.
    - Implemented user query and mutation functions for fetching and adding users.
    - Updated pnpm-lock.yaml to include Convex and related dependencies.
    - Created .gitignore for backend package to exclude local environment files.
    - Generated API and server utilities for Convex functions.
    - Configured TypeScript settings for backend package.
    - Added README documentation for Convex functions usage.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,19 @@
-import { add } from "@workspace/math/add"
-import { Button } from "@workspace/ui/components/button"
+"use client";
+import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
+import { useMutation, useQuery } from "convex/react";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World app/web</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2,2)}</p>
+        <h1 className="text-2xl font-bold">app/web</h1>
+         <Button onClick={() => addUser()}>Add user</Button>
+        {JSON.stringify(users)}
       </div>
+     
     </div>
-  )
+  );
 }
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,19 @@
-import { Button } from "@workspace/ui/components/button"
-import { Input } from "@workspace/ui/components/input"
+"use client";
+import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
+import { useMutation, useQuery } from "convex/react";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World app/widget</h1>
-        <Button size="sm">Button</Button>
-        <Input />
+        <h1 className="text-2xl font-bold">app/web</h1>
+         <Button onClick={() => addUser()}>Add user</Button>
+        {JSON.stringify(users)}
       </div>
+     
     </div>
-  )
+  );
 }
