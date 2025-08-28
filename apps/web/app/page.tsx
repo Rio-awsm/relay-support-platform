d8e891ebb61d9e9ce28e216d93e6e306d6458e33
Rio-awsm <mm.saha1982@gmail.com>
2025-08-28 18:50:17 +0530

    feat: integrate Clerk authentication and create auth layouts

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,19 +1,35 @@
 "use client";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
-import { useMutation, useQuery } from "convex/react";
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">app/web</h1>
-         <Button onClick={() => addUser()}>Add user</Button>
-        {JSON.stringify(users)}
-      </div>
-     
-    </div>
+    <>
+      <Authenticated>
+        <div className="flex items-center justify-center min-h-svh">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className="text-2xl font-bold">
+              app/web <UserButton />
+            </h1>
+
+            <Button onClick={() => addUser()}>Add user</Button>
+            {JSON.stringify(users)}
+          </div>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        Must be signed in
+        <SignInButton />
+      </Unauthenticated>
+    </>
   );
 }
